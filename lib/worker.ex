f314f12478be047a8156ec8c947ddc109f6db082
John Mark Untalan <apeiron.jmunts@gmail.com>
2016-10-09 08:52:56 +0800

    done chapter 4 page 101

--- lib/worker.ex
+++ lib/worker.ex
@@ -1,4 +1,73 @@
 defmodule Metex.Worker do
+  use GenServer
+
+  @name MW
+
+  ## Client API
+  
+  def start_link(opts \\ []) do
+    GenServer.start_link(__MODULE__, :ok, opts ++ [name: MW])
+  end
+
+  def get_temperature(location) do
+    GenServer.call(@name, {:location, location})
+  end
+
+  def get_stats do
+    GenServer.call(@name, :get_stats)
+  end
+
+  def reset_stats do
+    GenServer.cast(@name, :reset_stats)
+  end
+
+  def stop do
+    GenServer.cast(@name, :stop)
+  end
+
+  ## Server API
+  
+  def handle_call({:location, location}, _from, stats) do
+    case temperature_of(location) do
+      {:ok, temp} ->
+        new_stats = update_stats(stats, location)
+        {:reply, "#{temp}°C", new_stats}
+      _ ->
+        {:reply, :error, stats}
+    end
+  end
+
+  def handle_call(:get_stats, _from, stats) do
+    {:reply, stats, stats}
+  end
+
+  def handle_cast(:reset_stats, _stats) do
+    {:noreply, %{}}
+  end
+
+  def handle_cast(:stop, stats) do
+    {:stop, :normal, stats}
+  end
+
+  def handle_info(msg, stats) do
+    IO.puts "received #{inspect msg}"
+    {:noreply, stats}
+  end
+
+  ## Server Callbacks
+
+  def init(:ok) do
+    {:ok, %{}}
+  end
+
+  def terminate(reason, _stats) do
+    # We could write to a file, database, etc
+    IO.puts "server terminated because of #{inspect reason}"
+    :ok
+  end
+
+  ## Helper Functions
+
   def loop do
     receive do
       {sender_pid, location} -> send(sender_pid, {:ok, temperature_of(location)})
@@ -9,22 +78,14 @@ defmodule Metex.Worker do
   end
 
   defp temperature_of(location) do
-    result = url_for(location) |> HTTPoison.get |> parse_response
-    case result do
-      {:ok, temp} ->
-        "#{location}: #{temp}C"
-      :error ->
-        "#{location} not found"
-    end
+    location = URI.encode(location)
+    url_for(location) |> HTTPoison.get |> parse_response
   end
 
   defp url_for(location) do
-    location = URI.encode(location)
     "http://api.openweathermap.org/data/2.5/weather?q=#{location}&appid=#{apikey}"
   end
-
-  defp parse_response({:ok, %HTTPoison.Response{body: body, status_code: 200}}) do
-    body |> JSON.decode! |> compute_temperature
+defp parse_response({:ok, %HTTPoison.Response{body: body, status_code: 200}}) do body |> JSON.decode! |> compute_temperature
   end
 
   defp parse_response(_) do
@@ -41,6 +102,15 @@ defmodule Metex.Worker do
   end
 
   defp apikey do
-    System.get_env("openweathermap_apikey")
+    System.get_env("OPENWEATHERMAP_APIKEY")
+  end
+
+  defp update_stats(old_stats, location) do
+    case Map.has_key?(old_stats, location) do
+      true ->
+        Map.update!(old_stats, location, &(&1 + 1))
+      false ->
+        Map.put_new(old_stats, location, 1)
+    end
   end
 end
